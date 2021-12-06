d70ee444781387e9a7feccb6e1464c5891e41c2f
Tom Evans <tevans.uk@googlemail.com>
2021-12-06 12:13:23 +0000

    Add project structure and commands

---
+++ README.md
@@ -0,0 +1,68 @@
+# 2021 Advent of Code Python template
+
+This is a small python project to automate setting up each days challenges.
+
+It also contains my solutions for the 2021 challenge on the branch `tom`
+
+## Creating stubs for a day
+
+Run `poetry run mkday N`. This will create the following files (eg for day 5):
+
+* `aoc/five.py`
+* `tests/test_five.py`
+* `tests/five.txt`
+
+If you have `EDITOR=vim` in your environment, it will also open those files.
+
+Paste the days sample input in to the test data file, and then save your personal days' input to `resources/five.txt`
+
+The days stubs will look like this:
+
+```python
+from dataclasses import dataclass
+from pathlib import Path
+
+
+def main(datafile: Path):
+    data = parse_data(datafile)
+    print(f"Q1: {data}")
+    print(f"Q2: {data}")
+
+
+def parse_data(datafile: Path):
+    with datafile.open() as fp:
+        return [line for line in fp.readlines()]
+```
+
+```python
+from pathlib import Path
+
+import pytest
+
+from aoc.five import *
+
+
+@pytest.fixture
+def data():
+    return parse_data(Path(__file__).parent / "five.txt")
+
+
+def test_parse_data(data):
+    assert len(data) == 0
+```
+
+## Running a day's code
+
+To run your code, run `poetry run day N`. This will call the `main` method in
+that days module, with the path to that days input as the argument.
+
+To run the test code, run `poetry run pytest`.
+
+## Installation
+
+Requirements:
+
+* Python 3.10
+* Poetry
+
+Install the project requirements with `poetry install`.
---
+++ aoc/day.py
@@ -0,0 +1,78 @@
+import importlib
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+from num2words import num2words
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: poetry day [N]")
+        sys.exit(-1)
+    day = int(sys.argv[1])
+    print(f"Yo its day {day}")
+    module_name = f"aoc.{num2words(day)}"
+    input_file = Path(__file__).parent.parent / "resources" / f"{num2words(day)}.txt"
+    try:
+        mod = importlib.import_module(module_name)
+        func = getattr(mod, "main")
+    except (ModuleNotFoundError, AttributeError):
+        print(f"No such module/function {module_name}.main")
+        sys.exit(-1)
+    print(f"Running {module_name}.main")
+    sys.exit(func(input_file))
+
+
+def make_my_day():
+    if len(sys.argv) != 2:
+        print("Usage: poetry mkday [N]")
+        sys.exit(-1)
+    dayw = num2words(int(sys.argv[1]))
+    root = Path(__file__).parent.parent
+    pyfile = root / "aoc" / f"{dayw}.py"
+    if not pyfile.exists():
+        pyfile.write_text(
+            """from dataclasses import dataclass
+from pathlib import Path
+
+
+def main(datafile: Path):
+    data = parse_data(datafile)
+    print(f"Q1: {{data}}")
+    print(f"Q2: {{data}}")
+
+
+def parse_data(datafile: Path):
+    with datafile.open() as fp:
+        return [line for line in fp.readlines()]
+"""
+        )
+    testfile = root / "tests" / f"test_{dayw}.py"
+    if not testfile.exists():
+        testfile.write_text(
+            f"""from pathlib import Path
+
+import pytest
+
+from aoc.{dayw} import *
+
+
+@pytest.fixture
+def data():
+    return parse_data(Path(__file__).parent / "{dayw}.txt")
+
+
+def test_parse_data(data):
+    assert len(data) == 0
+"""
+        )
+    testdata = root / "tests" / f"{dayw}.txt"
+    if not testdata.exists():
+        testdata.write_text("")
+    files = [pyfile, testfile, testdata]
+    if os.environ.get("EDITOR") == "vim":
+        subprocess.check_call(["vim", "-o", *files])
+    else:
+        print("Created the following files:", *files, sep="\n")
